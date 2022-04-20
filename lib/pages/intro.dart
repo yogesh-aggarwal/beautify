887c51c1adacd369564cf798bb69371a49040fa0
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2022-04-20 12:23:08 +0530

    Added IntroPage layout

--- lib/pages/intro.dart
+++ lib/pages/intro.dart
@@ -1,15 +1,46 @@
+import 'package:beautify/widgets/body.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({Key? key}) : super(key: key);
 
+  navigateToHomePage(BuildContext context) {}
+
   Widget _buildBody(BuildContext context) {
-    return "Hello Body".text.make();
+    return BeautifyBody(
+      hPadding: 4.0,
+      child: SizedBox.fromSize(
+        size: Size.fromHeight(MediaQuery.of(context).size.height),
+        child: [
+          SizedBox.square(
+            dimension: MediaQuery.of(context).size.width,
+            child: Image.asset(
+              "assets/images/intro.png",
+              fit: BoxFit.contain,
+            ),
+          ),
+          [
+            "Let Us Reward".text.size(38).medium.make(),
+            [
+              "You,".text.size(38).medium.make().pOnly(right: 6),
+              "Naturally!!".text.size(38).bold.make(),
+            ].hStack().pOnly(top: 10),
+          ].vStack().pOnly(top: 30, bottom: 50),
+          Icon(
+            Icons.arrow_forward_ios_outlined,
+            size: 30,
+          ).p(25).box.border(color: Colors.black, width: 2.0).roundedLg.make(),
+        ].vStack(
+          axisSize: MainAxisSize.max,
+          alignment: MainAxisAlignment.center,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: _buildBody(context)));
+    return Scaffold(body: _buildBody(context));
   }
 }
--- lib/widgets/body.dart
+++ lib/widgets/body.dart
@@ -4,8 +4,13 @@ import 'package:velocity_x/velocity_x.dart';
 // ignore: non_constant_identifier_names
 Widget BeautifyBody({
   required Widget child,
+  double hPadding = 16.0,
+  double vPadding = 16.0,
 }) {
   return [
-    [child].vStack(crossAlignment: CrossAxisAlignment.start).px16().expand(),
-  ].hStack().py16().scrollVertical();
+    [child]
+        .vStack(crossAlignment: CrossAxisAlignment.start)
+        .pSymmetric(h: hPadding)
+        .expand(),
+  ].hStack().pSymmetric(v: vPadding).scrollVertical();
 }
