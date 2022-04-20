63d1bebf9799577b69f39b690c048b3c8e9520e4
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2022-04-20 12:55:46 +0530

    Added product_card widget file

---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+
+class ProductCardWiget extends StatelessWidget {
+  const ProductCardWiget({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
